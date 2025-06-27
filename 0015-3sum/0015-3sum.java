7927187c467ee1cbd79e5693f1b6edaea24cd564
Nishant Sharma <116968633+nishant-2411@users.noreply.github.com>
2025-06-27 11:20:00 +0530

    Time: 0 ms (100%), Space: 51.4 MB (83.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,49 @@
+import java.util.AbstractList;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
+        return new AbstractList<List<Integer>>() {
+
+            List<List<Integer>> ans;
+
+            @Override
+            public int size() {
+                if (ans == null)
+                    ans = createList(nums);
+                return ans.size();
+            }
+
+            @Override
+            public List<Integer> get(int index) {
+                if (ans == null)
+                    ans = createList(nums);
+                return ans.get(index);
+            }
+        };
+    }
+    public static List<List<Integer>> createList(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        Arrays.sort(nums);
+        int i = 0;
+        int n = nums.length;
+        while (i < n - 2) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                i++;
+            else {
+                int j = i + 1, k = n - 1;
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == 0) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        j++;
+                        while (j < k && nums[j] == nums[j - 1])
+                            j++;
+                    } else if (sum < 0)
+                        j++;
+                    else
+                        k--;
                 }
+                i++;
             }
         }
 
