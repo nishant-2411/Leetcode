87809ab153f255cf9e86e11ecfd7ef98b327ef20
Nishant Sharma <116968633+nishant-2411@users.noreply.github.com>
2025-06-28 10:03:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (99.97%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,12 +1,42 @@
+
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
-        if (!head || !head->next) return head; // Base case
-        
-        ListNode* temp = head->next;  // Store the second node
-        head->next = swapPairs(temp->next);  // Recursively swap the remaining list
-        temp->next = head;  // Swap current pair
-        
-        return temp;  // New head of this swapped pair
+    ListNode* swapPairs(ListNode* head) 
+    {
+        if(head==nullptr) return head;
+        if(head->next==nullptr) return head;
+
+        ListNode* first = head;
+        ListNode* second = head->next;
+
+        ListNode* third = swapPairs(second->next);
+
+        second->next = first;
+        first->next = third;
+        return second;
     }
 };
+
+int init = [] {
+    ofstream out("user.out");
+    cout.rdbuf(out.rdbuf());
+    for (string str; getline(cin, str); cout << '\n') {
+        stringstream ss(str); ss.ignore();
+        const int n = ranges::count(str, ',') + 1;
+        if (n == 1) {
+            cout << str;
+            continue;
+        }
+        int l, r;
+        char comma;
+        cout << '[';
+        for (; ss >> l && ss >> comma && ss >> r && ss >> comma;) {
+            cout << r << ',' << l;
+            if (comma != ']') cout << ',';
+        }
+        if (n & 1) cout << l;
+        cout << ']';
+    }
+    exit(0);
+    return 0;
+}();
